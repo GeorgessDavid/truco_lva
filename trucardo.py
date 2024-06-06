61ab4048cefe3c4c37bff8521ce266b7b346424f
ivodiazdev <ivandiazdev@gmail.com>
2024-06-06 19:59:05 -0300

    cambio funcion de determinar ganador y cada que una persona tira una carta, se descarta de la mano

--- trucardo.py
+++ trucardo.py
@@ -1,6 +1,9 @@
 import random
 #trucardo
 # Definimos los palos y los valores de las cartas
+turno = 1
+rondasGanadasjugador1 = 0
+rondasGanadasjugador2 = 0
 palos = ['Espadas', 'Bastos', 'Oros', 'Copas']
 valores = ['1', '2', '3', '4', '5', '6', '7', '10', '11', '12']
 
@@ -22,23 +25,6 @@ JERARQUIA = [
     ['4 de Espadas', '4 de Oros', '4 de Copas', '4 de Bastos']
 ]
 
-""" JERARQUIA = {
-    '1 Espadas': 14,
-    '1 Bastos': 13,
-    '7 Espadas': 12,
-    '7 Oros': 11,
-    '3': 10,
-    '2': 9,
-    '1': 8,
-    '12': 7,
-    '11': 6,
-    '10': 5,
-    '7': 4,
-    '6': 3,
-    '5': 2,
-    '4': 1
-} """
-
 # Creamos el mazo de cartas
 mazo = []
 for i in palos:
@@ -59,15 +45,29 @@ def repartir_cartas():
     mano_jugador2.append(mazo[3])
     mano_jugador2.append(mazo[4])
     mano_jugador2.append(mazo[5])
-    return mano_jugador1, mano_jugador2
+    return [mano_jugador1, mano_jugador2]
 
-# Función para determinar el ganador de una ronda
+
+
 def determinar_ganador(carta1, carta2):
-    valor_carta1 = JERARQUIA.get(carta1.split()[0], 0)
-    valor_carta2 = JERARQUIA.get(carta2.split()[0], 0)
-    if valor_carta1 > valor_carta2:
+    valor_carta1 = 0
+    valor_carta2 = 0
+    # Función para determinar el ganador de una ronda
+    for i in range(len(JERARQUIA)):
+        for j in range(len(JERARQUIA[i])):
+            if carta1 == JERARQUIA[i][j]:
+                valor_carta1 = i
+
+    # Encontrar índice de carta2
+    for i in range(len(JERARQUIA)):
+        for j in range(len(JERARQUIA[i])):
+            if carta1 == JERARQUIA[i][j]:
+                valor_carta2 = i
+    
+    # Devolver el mayor índice
+    if valor_carta1 < valor_carta2:
         return "Jugador 1"
-    elif valor_carta2 > valor_carta1:
+    elif valor_carta2 < valor_carta1:
         return "Jugador 2"
     else:
         return "Empate"
@@ -85,10 +85,12 @@ def jugar_truco():
         if(cartaJugada != 0 and cartaJugada != 1 and cartaJugada != 2):
             print('posicion no valida')
     # Jugar una ronda simple (primera carta de cada jugador)
-    ganador = determinar_ganador(mano_jugador1[0], mano_jugador2[0])
-    # print(f"Jugador 1 juega: {mano_jugador1[0]}")
-    # print(f"Jugador 2 juega: {mano_jugador2[0]}")
+    ganador = determinar_ganador(mano_jugador1[cartaJugada], mano_jugador2[0])
+    print(f"Jugador 1 juega: {mano_jugador1[cartaJugada]}")
+    print(f"Jugador 2 juega: {mano_jugador2[0]}")
+    mano_jugador1.remove(mano_jugador1[cartaJugada])
+    mano_jugador2.remove(mano_jugador2[0])
     # print(f"El ganador de la ronda es: {ganador}")
 
 # Ejecutar el juego
-# jugar_truco()
+jugar_truco()
