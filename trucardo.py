e8aad4cf840cfeca220bb157294c32f087dd3e40
GeorgessDavid <georgessdavid@gmail.com>
2024-06-07 17:28:42 -0300

    changes_calcularTanto

--- trucardo.py
+++ trucardo.py
@@ -46,6 +46,12 @@ for i in palos:
         carta = j+' de '+i
         mazo.append(carta)
 
+# Función para definir la jerarquía de las cartas.
+def obtener_jerarquia(carta):
+    for i in range(len(JERARQUIA)):
+        for j in range(len(JERARQUIA[i])):
+            if JERARQUIA[i][j] == carta:
+                return i
 # Función para barajar y repartir las cartas
 def repartir_cartas():
     random.shuffle(mazo)
@@ -63,15 +69,114 @@ def repartir_cartas():
 
 # Función para determinar el ganador de una ronda
 def determinar_ganador(carta1, carta2):
-    valor_carta1 = JERARQUIA.get(carta1.split()[0], 0)
-    valor_carta2 = JERARQUIA.get(carta2.split()[0], 0)
-    if valor_carta1 > valor_carta2:
+    valor_carta1 = obtener_jerarquia(carta1)
+    valor_carta2 = obtener_jerarquia(carta2)
+    # valor_carta1 = JERARQUIA.get(carta1.split()[0], 0)
+    # valor_carta2 = JERARQUIA.get(carta1.split()[0], 0)
+    if valor_carta1 < valor_carta2:  #Como utilizamos los índices para determinar cuál es la carta más alta, cuanto más bajo el número del valor, más alto es el valor de la carta.
         return "Jugador 1"
-    elif valor_carta2 > valor_carta1:
+    elif valor_carta2 < valor_carta1:
         return "Jugador 2"
     else:
         return "Empate"
 
+# Función para descartar la carta jugada.
+def carta_jugada(carta, mazo):
+    while len(mazo) > 0 and len(carta) > 0:
+        for i in range(len(mazo)):
+            if carta == mazo[i]:
+                cartasEnMano = mazo
+                mazo.remove(carta)
+                return cartasEnMano
+            else:
+                return 'Error, se esperaba que ingrese la carta jugada y la mano del jugador.'
+    else:
+        return 'Error inesperado, se esperaba un dato tipo String y un Arreglo.'
+    
+
+# Función para separar el tanto recibido, devuelve número como integer y el palo de la carta.
+def separar_tanto(string):
+    numero = ''
+    palo = ''
+    
+    partes = string.split(' de ')
+    numero = int(partes[0])
+    palo = partes[1]
+
+    return [numero, palo]
+
+# Función para calcular el tanto de la mano.
+def calcular_tanto(cartas):
+    while len(cartas) >= 2: 
+        num = []
+        palo = []
+        # separamos los números y los palos
+        for i in range(len(cartas)):
+            naipe = separar_tanto(cartas[i])
+            num.append(naipe[0])
+            palo.append(naipe[1])
+
+        tanto = 0
+
+        # Caso particular de que las 3 cartas sean del mismo palo ya que no se utiliza la flor.
+        if palo[0] == palo[1] == palo[2]:
+            masAlta1 = num[0]
+            masAlta2 = num[1]
+            if num[1] > masAlta1:
+                masAlta1 = num[1]
+                masAlta2 = num[0]
+            if num[2] > masAlta1:
+                masAlta2 = masAlta1
+                masAlta1 = num[2]
+            elif num[2] > masAlta2:
+                masAlta2 = num[2]
+                tanto = masAlta1 + masAlta2
+                return tanto
+        
+        # Caso 1: Dos cartas del mismo palo y sin figuras
+        if palo[0] == palo[1] and num[0] < 10 and num[1] < 10:
+            tanto = 20 + num[0] + num[1]
+        elif palo[0] == palo[2] and num[0] < 10 and num[2] < 10:
+            tanto = 20 + num[0] + num[2]
+        elif palo[1] == palo[2] and num[1] < 10 and num[2] < 10:
+            tanto = 20 + num[1] + num[2]
+
+        # Caso 2 Dos cartas del mismo palo y con almenos una figura
+    
+        elif palo[0] == palo[1]:
+            if num[0] > 10 and num[1] > 10:
+                tanto = 20
+            elif num[0] > 10:
+                tanto = 20 + num[1]
+            elif num[1] > 10:
+                tanto = 20 + num[0]
+        elif palo[0] == palo[2]:
+            if num[0] > 10 and num[2] > 10:
+                tanto = 20
+            elif num[0] > 10:
+                tanto = 20 + num[2]
+            elif num[2] > 10:
+                tanto = 20 + num[0]
+        elif palo[1] == palo[2]:
+            if num[1] > 10 and num[2] > 10:
+                tanto = 20
+            elif num[1] > 10:
+                tanto = 20 + num[2]
+            elif num[2] > 10:
+                tanto = 20 + num[1]
+
+        # Caso 3 Ningún par de cartas del mismo palo
+        else:
+            if num[0] < 10 and num[0] >= num[1] and num[0] >= num[2]:
+                tanto = num[0]
+            elif num[1] < 10 and num[1] >= num[0] and num[1] >= num[2]:
+                tanto = num[1]
+            elif num[2] < 10 and num[2] >= num[0] and num[2] >= num[1]:
+                tanto = num[2]
+            
+        return tanto
+    else:
+        print('Error, se esperaba un arreglo de dos o más cartas')
 # Función principal del juego
 def jugar_truco():
     cartas = repartir_cartas()
@@ -91,4 +196,4 @@ def jugar_truco():
     # print(f"El ganador de la ronda es: {ganador}")
 
 # Ejecutar el juego
-# jugar_truco()
+jugar_truco()
